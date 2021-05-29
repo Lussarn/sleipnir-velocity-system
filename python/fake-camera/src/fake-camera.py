776db2cc6c597fee87d93fc512a8a00e5212cab1
Linus Larsson <linus.larsson@gmail.com>
2021-05-29 10:51:44 +0200

    fake cameras sending startcamera requests

--- python/fake-camera/src/fake-camera.py
+++ python/fake-camera/src/fake-camera.py
@@ -8,9 +8,27 @@ class Camera:
     STATE_IDLE = 0
     STATE_UPLOADING = 1
 
-    def __init__(self):
-        self.state = Camera.STATE_IDLE
+    def __init__(self, cam):
+        self.__state = Camera.STATE_IDLE
+        self.__cam = cam
+
+    def get_state(self):
+        return self.__state
+
+    def get_cam(self):
+        return self.__cam
+
+cams = [
+    Camera('cam1'),
+    Camera('cam2'),
+]
+
 
 while True:
-    response = requests.post(url, data = {'action': 'startcamera', 'id': 'cam1'}, timeout=1)
-    time.sleep(1)
+    for cam_idx in range(2):
+        cam = cams[cam_idx]
+
+        if cams[cam_idx].get_state() == Camera.STATE_IDLE:
+            response = requests.post(url, data = { 'action': 'startcamera', 'id': cam.get_cam() }, timeout=1)
+
+    time.sleep(0.011)
