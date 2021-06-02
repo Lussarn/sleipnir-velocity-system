56909ddc9215644aeee42f1bd4034879645ac454
Linus Larsson <linus.larsson@gmail.com>
2021-06-02 09:14:16 +0200

    implemented tornado

--- python/sleipnir-base/src/CameraServer.py
+++ python/sleipnir-base/src/CameraServer.py
@@ -17,11 +17,13 @@ from Frame import Frame
 from database.DB import DB
 import database.frame_dao as frame_dao
 from function_timer import timer
-
-from multiprocessing import Process, Pipe
+import asyncio
+import tornado.ioloop
+import tornado.web
 
 logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger('tornado.access').disabled = True
 logger = logging.getLogger(__name__)
 
 class ServerData:
@@ -40,22 +42,28 @@ class ServerData:
 class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
    """Handle requests in a separate thread."""
 
-class SleipnirRequestHandler(http.server.SimpleHTTPRequestHandler):
+#class SleipnirRequestHandler(http.server.SimpleHTTPRequestHandler):
+class TornadoHandler(tornado.web.RequestHandler):
    def log_message(self, format, *args):
       pass
 
    @timer("Http POST", logging.INFO, identifier=None, average=1000)
-   def do_POST(self):
+#   def do_POST(self):
+   def post(self):
       global ServerData
 
-      ctype, pdict = cgi.parse_header(self.headers['content-type'])
-      if ctype == 'multipart/form-data':
-         postvars = cgi.parse_multipart(self.rfile, pdict)
-      elif ctype == 'application/x-www-form-urlencoded':
-         length = int(self.headers['content-length'])
-         postvars = urllib.parse.parse_qs(self.rfile.read(length), keep_blank_values=1)
-      else:
-         postvars = {}
+
+      ctype, pdict = cgi.parse_header(self._headers['content-type'])
+      postvars = urllib.parse.parse_qs(self.request.body, keep_blank_values=1)
+#      if ctype == 'multipart/form-data':
+#         postvars = cgi.parse_multipart(self.rfile, pdict)
+#         print("BODY")
+#         print(self.request.body)
+#      elif ctype == 'application/x-www-form-urlencoded':
+#         length = int(self.headers['content-length'])
+#         postvars = urllib.parse.parse_qs(self.rfile.read(length), keep_blank_values=1)
+#      else:
+#         postvars = {}
 
       action = postvars[b'action'][0].decode('utf-8')
 
@@ -109,19 +117,29 @@ class SleipnirRequestHandler(http.server.SimpleHTTPRequestHandler):
             self.send200("OK-STOP")
 
    def send200(self, msg):
-      self.send_response(200)
-      self.send_header('Content-Type', 'text/plain')
+#      self.send_response(200)
+      self.set_status(200)
+#      self.set
+#      self.send_header('Content-Type', 'text/plain')
+      self.set_header('Content-Type', 'text/plain')
       payload = msg.encode('ASCII')
+      self.write(payload)
 #      self.send_header('Content-Length: ', str(len(payload)))
-      self.end_headers()
-      self.wfile.write(payload)
-      self.wfile.flush()
+#      self.end_headers()
+#      self.wfile.write(payload)
+#      self.wfile.flush()
 
 
 
 def __startHTTP():
-   server = ThreadingSimpleServer(('', 8000), SleipnirRequestHandler)
-   server.serve_forever()
+#   server = ThreadingSimpleServer(('', 8000), SleipnirRequestHandler)
+#   server.serve_forever()
+   asyncio.set_event_loop(asyncio.new_event_loop())
+   app = tornado.web.Application([
+      (r"/", TornadoHandler),
+   ])
+   app.listen(8000)
+   tornado.ioloop.IOLoop.instance().start()
 
 def is_shooting():
    global ServerData
