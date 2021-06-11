4dc83d87b96e49a0d8041a27210609b7edd156f2
Linus Larsson <linus.larsson@gmail.com>
2021-06-11 17:51:28 +0200

    cross compile pod

---
+++ sleipnir-pod/sleipnir/cross-compile-readme.txt
@@ -0,0 +1,6 @@
+
+ON PI:
+sudo tar -cvzf rootfs.tar.gz usr
+
+ON WIN:
+scp pi@vdev:/rootfs.tar.gz C:\rasperrypi\rootfs\
---
+++ sleipnir-pod/sleipnir/cross-compile-windows.ps1
@@ -0,0 +1,48 @@
+$SYS_GCC_RASPBERRY = "C:\SysGCC\raspberry"
+$ROOT = "C:\raspberrypi\rootfs"
+
+$GCC = $SYS_GCC_RASPBERRY + "\bin\arm-linux-gnueabihf-gcc.exe"
+$GPP = $SYS_GCC_RASPBERRY + "\bin\arm-linux-gnueabihf-c++.exe"
+$LD = $SYS_GCC_RASPBERRY + "\bin\arm-linux-gnueabihf-ld.exe"
+
+
+Write-Output "Compiling sleipnir.c"
+& $GCC -c sleipnir.c `
+    -I"$ROOT"\usr\include `
+    -I"$ROOT"\usr\include\arm-linux-gnueabihf `
+    -I"$ROOT"\opt\vc\include `
+    -I"$ROOT"\opt\vc\include\interface\vcos\pthreads `
+    -I"$ROOT"\opt\vc\include\interface\vmcs_host\linux
+If ($lastExitCode -ne "0") {
+    exit $lastExitCode 
+}
+
+Write-Output "Compiling RaspiCamControl.c"
+& $GCC -c RaspiCamControl.c `
+    -I"$ROOT"\usr\include `
+    -I"$ROOT"\usr\include\arm-linux-gnueabihf `
+    -I"$ROOT"\opt\vc\include `
+    -I"$ROOT"\opt\vc\include\interface\vcos\pthreads `
+    -I"$ROOT"\opt\vc\include\interface\vmcs_host\linux
+If ($lastExitCode -ne "0") {
+    exit $lastExitCode 
+}
+    
+Write-Output "Compiling RaspiCLI.c"
+& $GCC -c RaspiCLI.c `
+    -I"$ROOT"\usr\include `
+    -I"$ROOT"\usr\include\arm-linux-gnueabihf `
+    -I"$ROOT"\opt\vc\include `
+    -I"$ROOT"\opt\vc\include\interface\vcos\pthreads `
+    -I"$ROOT"\opt\vc\include\interface\vmcs_host\linux
+If ($lastExitCode -ne "0") {
+    exit $lastExitCode 
+}
+    
+Write-Output "Linking files..."
+& $GCC --sysroot="$ROOT" `
+    -o sleipnir-pod sleipnir.o RaspiCamControl.o RaspiCLI.o `
+    "-Wl,-rpath-link=$ROOT\usr\lib\arm-linux-gnueabihf" `
+    "-Wl,-rpath-link=$ROOT\opt\vc\lib" `
+    -L"$ROOT"\opt\vc\lib `
+    -lvcos -lbcm_host -lturbojpeg -lb64 -lcurl -lpthread -lmmal_core -lmmal -lmmal_components -lmmal_util
