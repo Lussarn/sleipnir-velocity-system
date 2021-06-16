f66ce5e3ea93435432725532d501a620abd43082
Linus Larsson <linus.larsson@gmail.com>
2021-06-16 14:20:01 +0200

    mend

--- sleipnir-pod/sleipnir/RaspiCamControl.c
+++ sleipnir-pod/sleipnir/RaspiCamControl.c
@@ -170,7 +170,6 @@ int raspicamcontrol_set_all_parameters(MMAL_COMPONENT_T *camera, const RASPICAM_
    result += raspicamcontrol_set_awb_gains(camera, params->awb_gains_r, params->awb_gains_b);
    result += raspicamcontrol_set_imageFX(camera, params->imageEffect);
    result += raspicamcontrol_set_colourFX(camera, &params->colourEffects);
-   //result += raspicamcontrol_set_thumbnail_parameters(camera, &params->thumbnailConfig);  TODO Not working for some reason
    result += raspicamcontrol_set_rotation(camera, params->rotation);
    result += raspicamcontrol_set_flips(camera, params->hflip, params->vflip);
    result += raspicamcontrol_set_ROI(camera, params->roi);
